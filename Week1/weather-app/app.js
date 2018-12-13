14de068c4d6c5db9bfef7f1d489412059691e6d1
StevanCakic <stevancakic93@gmail.com>
2018-12-13 09:59:39 +0100

    code refactoring for weather app

--- Week1/weather-app/app.js
+++ Week1/weather-app/app.js
@@ -1,19 +1,29 @@
 const request = require("request");
 
-request({
-    url: "https://maps.googleapis.com/maps/api/geocode/json?address=Podgorica&key=AIzaSyBclVRrc8KvXbAWNYjLYOV_CA3k0YR9XP4",
-    json: true // saljemo zahtjev i u request headeru dodajemo accept:application/json
-}, (error, response, body) => {
-    console.log("RESPONSE");
-    console.log(JSON.stringify(response, undefined, 2));
-    /*
-    console.log("BODY");
-    console.log(JSON.stringify(body, undefined, 2));
-    console.log("ERROR");
-    console.log(JSON.stringify(error, undefined, 2));*/
+const yargs = require("yargs");
 
-    /*
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);*/
-})
+const geocode = require("./geocode/geocode");
+
+const addressCommand = {
+    demand: true,
+    alias: "address",
+    describe: "Address to fetch weather for",
+    string: true //parse adress value to string
+}
+
+const argv = yargs
+    .options({
+        a: addressCommand
+    })
+    .help()
+    .alias("help", "h")
+    .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
+    }
+});
+
---
+++ Week1/weather-app/geocode/geocode.js
@@ -0,0 +1,43 @@
+const request = require("request");
+
+function geocodeAddress(address, cb) {
+    const encodedAddress = encodeURIComponent(address); // moze i a
+
+    request({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBclVRrc8KvXbAWNYjLYOV_CA3k0YR9XP4`,
+        json: true // saljemo zahtjev i u request headeru dodajemo accept:application/json
+    }, (error, response, body) => {
+        /*
+        console.log("RESPONSE");
+        console.log(JSON.stringify(response, undefined, 2));*/
+
+        /*
+        console.log("BODY");
+        console.log(JSON.stringify(body, undefined, 2));
+        console.log("ERROR");
+        console.log(JSON.stringify(error, undefined, 2));*/
+
+        if (error) {
+            cb("Unable to connect to Google servers.")
+            // console.log("Unable to connect to Google servers.")
+        } else if (body.status === "ZERO_RESULTS") {
+            cb("Unable to find that address")
+            // console.log("Unable to find that address");
+        } else if (body.status === "OK") {
+            cb(undefined, {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+            });
+            /*
+            console.log(`Address: ${body.results[0].formatted_address}`);
+            console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+            console.log(`Longitude: ${body.results[0].geometry.location.lng}`);*/
+        }
+
+    })
+}
+
+module.exports = {
+    geocodeAddress
+}
